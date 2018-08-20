25b28d899ca9f0cbdaa3eeb7391de6ee3c985802
Alexandru Giurgiu <alex.giurgiu@gmail.com>
2018-08-20 16:39:03 +0200

    moved the dashboard related subs to the new directory

---
+++ src/protosfrontend/dashboard/subs.cljs
@@ -0,0 +1,42 @@
+(ns dashboard.subs
+    (:require
+        [re-frame.core :as rf]
+        [clairvoyant.core :refer-macros [trace-forms]]
+        [re-frame-tracer.core :refer [tracer]]))
+
+(trace-forms {:tracer (tracer :color "blue")}
+
+(rf/reg-sub
+  :apps
+  (fn apps-sub
+    [db _]
+    (-> db
+        :apps)))
+
+(rf/reg-sub
+  :installers
+  (fn installers-sub
+    [db _]
+    (-> db
+        :installers)))
+
+(rf/reg-sub
+  :installer
+  (fn installer-sub
+    [db [_ installer-id]]
+    (get-in db [:installers (keyword installer-id)])))
+
+(rf/reg-sub
+  :installer-params
+  (fn installer-params-sub
+    [db [_ installer-id]]
+    (get-in db [:installers (keyword installer-id) :metadata :params])))
+
+(rf/reg-sub
+  :resources
+  (fn resources-sub
+    [db _]
+    (-> db
+        :resources)))
+
+)
