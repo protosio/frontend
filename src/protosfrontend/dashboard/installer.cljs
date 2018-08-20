af8f2abe788a6cbc33ef787396049ab59733a0a9
Alexandru Giurgiu <alex.giurgiu@gmail.com>
2018-08-20 19:34:57 +0200

    new buttons for the installer page

--- src/protosfrontend/dashboard/installer.cljs
+++ src/protosfrontend/dashboard/installer.cljs
@@ -1,7 +1,6 @@
 (ns dashboard.installer
     (:require
-      [re-frame.core :as rf]
-      [baking-soda.core :as b]))
+      [re-frame.core :as rf]))
 
 (defn installer-list []
   [:div {:class "col-lg-12 grid-margin stretch-card"}
@@ -34,26 +33,28 @@
                       selected-version (last (sort versions))
                       metadata (get-in installer [:versions selected-version])]
                 [:div {:class "card-body"}
-                    [:h4 {:class "card-title"} (:name installer)]
-                    [:div.installer-details
-                        [:div.table-responsive
-                            [:table {:class "table table-striped table-bordered"}
-                             [:tbody
-                              [:tr
-                                [:th "ID"]
-                                [:td (:id installer)]]
-                              [:tr
-                               [:th "Name"]
-                               [:td (:name installer)]]
-                              [:tr
-                               [:th "Versions"]
-                               [:td (for [version versions]
-                                         [:button {:type "button" :key version :class (str "btn btn-sm btn-rounded submit-btn mr-1 "  (if (= selected-version version) "btn-inverse-dark" "btn-outline-dark"))} version])]]
-                              [:tr
-                               [:th "Description"]
-                               [:td (-> metadata :description)]]
-                              [:tr
-                               [:th "Provides"]
-                               [:td (clojure.string/join  " " (-> metadata :provides))]]]]]]
-                    [b/Button {:color "danger" :on-click #(rf/dispatch [:remove-installer id])} "Remove"]
-                    [b/Button {:color "primary" :on-click #(rf/dispatch [:open-modal :create-app-modal (:id installer)])} "Create app"]])]]])
+                  [:h2 {:class "card-title"} (:name installer)]
+                    [:div {:class "form-group"}
+                        [:div.installer-details
+                            [:div.table-responsive
+                                [:table {:class "table table-striped table-bordered"}
+                                 [:tbody
+                                  [:tr
+                                    [:th "ID"]
+                                    [:td (:id installer)]]
+                                  [:tr
+                                   [:th "Name"]
+                                   [:td (:name installer)]]
+                                  [:tr
+                                   [:th "Versions"]
+                                   [:td (for [version versions]
+                                             [:button {:type "button" :key version :class (str "btn btn-sm btn-rounded submit-btn mr-1 "  (if (= selected-version version) "btn-inverse-dark" "btn-outline-dark"))} version])]]
+                                  [:tr
+                                   [:th "Description"]
+                                   [:td (-> metadata :description)]]
+                                  [:tr
+                                   [:th "Provides"]
+                                   [:td (clojure.string/join  " " (-> metadata :provides))]]]]]]]
+                        [:div {:class "col-12"}
+                          [:button {:type "button" :class "btn btn-rounded btn-rounded btn-outline-danger mr-1" :on-click #(rf/dispatch [:remove-installer id])} "Delete"]
+                          [:button {:type "button" :class "btn btn-rounded btn-rounded btn-outline-primary mr-1" :on-click #(rf/dispatch [:remove-installer id])} "Create app"]]])]]])
