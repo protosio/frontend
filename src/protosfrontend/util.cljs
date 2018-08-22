5b1f87debd9176855fd89b2685dfc319dd6d8992
Alexandru Giurgiu <alex.giurgiu@gmail.com>
2018-08-22 15:48:33 +0200

    added form-events util function

---
+++ src/protosfrontend/util.cljs
@@ -0,0 +1,12 @@
+(ns protosfrontend.util
+    (:require
+        [re-frame.core :as rf]))
+
+(defn form-events
+  [dbpath]
+  {:get (fn [path] @(rf/subscribe [:form-field-value path]))
+   :save! (fn [path value] (rf/dispatch [:set-form-value path value]))
+   :update! (fn [path save-fn value]
+              (rf/dispatch [:set-form-value path
+                              (save-fn @(rf/subscribe [:form-field-value path]) value)]))
+   :doc (fn [] @(rf/subscribe dbpath))})
