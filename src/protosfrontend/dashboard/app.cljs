d911a8dba439aa8d68a6b8af5d530a54055da3d6
Alexandru Giurgiu <alex.giurgiu@gmail.com>
2018-09-15 03:54:34 +0300

    the apps page now display the applications correctly with the new template

--- src/protosfrontend/dashboard/app.cljs
+++ src/protosfrontend/dashboard/app.cljs
@@ -11,29 +11,56 @@
           [:div {:class "col-12"}
             [:div {:class (str "alert alert-" (:type alert-data)) :role "alert"} (:message alert-data)]]]])))
 
-(defn app-list []
-  [:div {:class "col-lg-12 grid-margin stretch-card"}
-    [:div {:class "card"}
-      [:div {:class "card-body"}
-        [:h4 {:class "card-title"} "Apps"]
+(defn apps-page [title]
+  [:div {:class "container"}
+    (if title
+      [:div.page-header [:h1.page-title title]])
+    [:div {:class "row row-cards row-deck"}
+      [:div {:class "col-12"}
+       [:div {:class "card"}
         [:div {:class "table-responsive"}
-          [:table {:class "table table-striped"}
+          [:table {:class "table table-hover table-outline table-vcenter text-nowrap card-table"}
             [:thead
               [:tr
+                [:th {:class "text-center w-1"}
+                  [:i {:class "icon-people"}]]
                 [:th "Name"]
-                [:th "ID"]
-                [:th "Status"]]]
+                [:th "Last activity"]
+                [:th {:class "text-center"} "Runtime"]
+                [:th "Status"]
+                [:th {:class "text-center"}
+                  [:i {:class "icon-settings"}]]]]
             [:tbody
             (let [apps @(rf/subscribe [:apps])]
               (for [{name :name, id :id, status :status} (vals apps)]
-                [:tr {:key id :style {:width "100%"}}
-                  [:td [:a {:href (str "/#/apps/" id)} name]]
-                  [:td id]
-                  [:td status]]))]]]]]])
-
-(defn apps-page []
-    [:div {:class "row"}
-        [app-list]])
+              [:tr {:key id}
+                [:td {:class "text-center"}
+                  [:div {:class "avatar d-block" :style {:background-image "url(images/app-generic.png)"}}
+                    [:span {:class "avatar-status bg-green"}]]]
+                [:td
+                  [:a {:href (str "/#/apps/" id)} name]
+                  [:div {:class "small text-muted"} (str "ID: " id)]]
+                [:td
+                  [:div {:class "small text-muted"} "Application started"]
+                  [:div "5 minutes ago"]]
+                [:td {:class "text-center"}
+                  [:i {:class "payment payment-visa"}]]
+                [:td
+                  [:div status]]
+                [:td
+                  [:div {:class "item-action dropdown"}
+                    [:a {:href "javascript:void(0)", :data-toggle "dropdown", :class "icon"}
+                      [:i {:class "fe fe-more-vertical"}]]
+                    [:div {:class "dropdown-menu dropdown-menu-right"}
+                      [:a {:href "javascript:void(0)", :class "dropdown-item"}
+                        [:i {:class "dropdown-icon fe fe-tag"}]" Action "]
+                      [:a {:href "javascript:void(0)", :class "dropdown-item"}
+                        [:i {:class "dropdown-icon fe fe-edit-2"}]" Another action "]
+                      [:a {:href "javascript:void(0)", :class "dropdown-item"}
+                        [:i {:class "dropdown-icon fe fe-message-square"}]" Something else here"]
+                      [:div {:class "dropdown-divider"}]
+                      [:a {:href "javascript:void(0)", :class "dropdown-item"}
+                        [:i {:class "dropdown-icon fe fe-link"}]" Separated link"]]]]]))]]]]]]])
 
 (defn app-page [id]
   [:div {:class "row"}
@@ -69,3 +96,5 @@
               [util/submit-button "Refresh" [:get-app app-id] "primary" loading?]
               [util/submit-button "Remove" [:remove-app app-id] "danger" loading?]]]
           [alert [:alert-dashboard]]])]]])
+
+
