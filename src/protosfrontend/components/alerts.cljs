50fdf582d206f165b4f7598563a0a02f6b41205b
Alexandru Giurgiu <alex.giurgiu@gmail.com>
2018-10-09 17:41:22 +0300

    added a couple of alert components

---
+++ src/protosfrontend/components/alerts.cljs
@@ -0,0 +1,17 @@
+(ns components.alerts
+    (:require
+      [re-frame.core :as rf]))
+
+(defn for-list-page [alert-sub]
+  (let [alert-data @(rf/subscribe alert-sub)]
+    (when alert-data
+      [:div {:class (str "alert alert-" (:type alert-data) " alert-dismissible mb-1")}
+        [:button {:type "button" :class "close" :data-dismiss "alert"}]
+        (:message alert-data)])))
+
+(defn for-card [alert-sub]
+  (let [alert-data @(rf/subscribe alert-sub)]
+    (when alert-data
+      [:div {:class (str "card-alert alert alert-" (:type alert-data) " alert-dismissible mb-0")}
+        [:button {:type "button" :class "close" :data-dismiss "alert"}]
+        (:message alert-data)])))
