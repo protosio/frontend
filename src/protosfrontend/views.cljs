2cc1eecdef4a53bbe8e81a29974d545dd54016a3
Alexandru Giurgiu <alex.giurgiu@gmail.com>
2018-10-13 17:54:21 +0300

    simplified the way the page components are loaded

--- src/protosfrontend/views.cljs
+++ src/protosfrontend/views.cljs
@@ -35,18 +35,18 @@
     [authviews/login-form]])
 
 (defn current-page []
-  (let [[active-page item-id & params]  @(rf/subscribe [:active-page])]
+  (let [[active-page _ item-id]  @(rf/subscribe [:active-page])]
     (condp = active-page
       :init-page            [init-page]
       :login-page           [login-page]
       :dashboard-page       [regular-page home/home-page "Dashboard" active-page]
-      :task-page            [regular-page #(apply task/task-page params)]
+      :task-page            [regular-page task/task-page item-id active-page]
       :tasks-page           [regular-page task/tasks-page "Tasks" active-page]
-      :app-page             [regular-page #(apply app/app-page params)]
+      :app-page             [regular-page app/app-page item-id active-page]
       :apps-page            [regular-page app/apps-page "Apps" active-page]
       :store-page           [regular-page store/store-page "Store" active-page]
-      :store-installer-page [regular-page #(apply store/store-installer-page params)]
+      :store-installer-page [regular-page store/store-installer-page item-id active-page]
       :resources-page       [regular-page resource/resources-page "Resources" active-page]
-      :resource-page        [regular-page #(apply resource/resource-page params)])))
+      :resource-page        [regular-page resource/resource-page item-id active-page])))
 
 )
