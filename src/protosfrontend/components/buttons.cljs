6a04b47f33613c5cd6015e6167a78dadc21d2903
Alexandru Giurgiu <alex.giurgiu@gmail.com>
2018-10-16 19:44:23 +0300

    the button components are not properly disabling as opposed to previously, where only the styling was set to disabled

--- src/protosfrontend/components/buttons.cljs
+++ src/protosfrontend/components/buttons.cljs
@@ -5,11 +5,13 @@
 (defn submit-button [text dispatch-value style disabled?]
   [:button {:type "button"
             :on-click #(rf/dispatch dispatch-value)
-            :class (str "btn btn-" style (when disabled? " disabled"))}
-            text])
+            :class (str "btn btn-" style )
+            :disabled (if disabled? true false)}
+           text])
 
 (defn submit-button-spinner [text dispatch-value style disabled? loading?]
   [:button {:type "button"
             :on-click #(rf/dispatch dispatch-value)
-            :class (str "btn btn-" style (when disabled? " disabled"))}
-            (if loading? [:i {:class "fa fa-spin fa-circle-o-notch"}] text)])
+            :class (str "btn btn-" style)
+            :disabled (if disabled? true false)}
+           (if loading? [:i {:class "fa fa-spin fa-circle-o-notch"}] text)])
