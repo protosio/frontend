dc36972f8e049f4a4cef765b6b4b5b190ac0cfe3
Alexandru Giurgiu <alex.giurgiu@gmail.com>
2018-11-07 17:57:21 +0200

    added task related information to the app listing page and app detail page

--- src/protosfrontend/dashboard/app.cljs
+++ src/protosfrontend/dashboard/app.cljs
@@ -21,45 +21,57 @@
               [:tr
                 [:th {:class "text-center w-1"}]
                 [:th "Name"]
-                [:th "Tasks"]
+                [:th "Task"]
                 [:th {:class "text-center"} "Runtime"]
                 [:th "Status"]
                 [:th {:class "text-center"}
                   [:i {:class "fe fe-settings"}]]]]
             [:tbody
-            (let [apps @(rf/subscribe [:apps])]
-              (for [{name :name id :id status :status} (vals apps)]
-              [:tr {:key id}
-                [:td {:class "text-center"}
-                  [:div {:class "avatar d-block bg-white" :style {:background-image "url(/static/images/app-generic.svg)" :background-size "80%"}}
-                    [:span {:class (str "avatar-status bg-" (util/app-status-color status))}]]]
-                [:td
-                  [:a {:href (routes/url-for :app-page :id id)} name]
-                  [:div {:class "small text-muted"} (str "ID: " id)]]
-                [:td
-                  [:div {:class "small text-muted"} "Application started"]
-                  [:div "5 minutes ago"]]
-                [:td {:class "text-center"}
-                  [:i {:class "tech tech-docker"}]]
-                [:td
-                  [:div status]]
-                [:td {:class "text-center"}
-                  [:div {:class "item-action dropdown"}
-                    [:a {:data-toggle "dropdown" :class "icon"}
-                      [:i {:class "fe fe-more-vertical"}]]
-                    [:div {:class "dropdown-menu dropdown-menu-right"}
-                      [:a {:href "javascript:void(0)" :on-click #(rf/dispatch [:app-state id "start"]) :class "dropdown-item"}
-                        [:i {:class "dropdown-icon fe fe-play"}] " Start"]
-                      [:a {:href "javascript:void(0)" :on-click #(rf/dispatch [:app-state id "stop"]) :class "dropdown-item"}
-                        [:i {:class "dropdown-icon fe fe-stop-circle"}] " Stop"]
-                      [:a {:href "javascript:void(0)" :on-click #(rf/dispatch [:remove-app id]) :class "dropdown-item"}
-                        [:i {:class "dropdown-icon fe fe-trash"}] " Remove"]]]]]))]]]]]]])
+            (let [apps @(rf/subscribe [:apps])
+                  apps-tasks @(rf/subscribe [:apps-tasks])]
+              (for [[app-id {name :name status :status id :id tasks :tasks}] apps
+                    :let [app-tasks (app-id apps-tasks)
+                          last-task (last (vals app-tasks))
+                          progress (:progress last-task)]]
+                [:tr {:key app-id}
+                  [:td {:class "text-center"}
+                    [:div {:class "avatar d-block bg-white" :style {:background-image "url(/static/images/app-generic.svg)" :background-size "80%"}}
+                      [:span {:class (str "avatar-status bg-" (util/app-status-color status))}]]]
+                  [:td
+                    [:a {:href (routes/url-for :app-page :id id)} name]
+                    [:div {:class "small text-muted"} (str "ID: " id)]]
+                  [:td
+                    [:div {:class "clearfix"} [:div {:class "float-left"} [:strong (str (:percentage progress) "%")]]
+                                              [:div {:class "float-right"} [:small {:class "text-muted"} (:name last-task)]]]
+                    [:div {:class "progress progress-xs"}
+                      [:div {:class "progress-bar bg-green"
+                             :aria-valuemax "100"
+                             :aria-valuemin "0"
+                             :aria-valuenow (:percentage progress)
+                             :style {:width (str (:percentage progress) "%")}
+                             :role "progressbar"}]]]
+                  [:td {:class "text-center"}
+                    [:i {:class "tech tech-docker"}]]
+                  [:td
+                    [:div status]]
+                  [:td {:class "text-center"}
+                    [:div {:class "item-action dropdown"}
+                      [:a {:data-toggle "dropdown" :class "icon"}
+                        [:i {:class "fe fe-more-vertical"}]]
+                      [:div {:class "dropdown-menu dropdown-menu-right"}
+                        [:a {:href "javascript:void(0)" :on-click #(rf/dispatch [:app-state id "start"]) :class "dropdown-item"}
+                          [:i {:class "dropdown-icon fe fe-play"}] " Start"]
+                        [:a {:href "javascript:void(0)" :on-click #(rf/dispatch [:app-state id "stop"]) :class "dropdown-item"}
+                          [:i {:class "dropdown-icon fe fe-stop-circle"}] " Stop"]
+                        [:a {:href "javascript:void(0)" :on-click #(rf/dispatch [:remove-app id]) :class "dropdown-item"}
+                          [:i {:class "dropdown-icon fe fe-trash"}] " Remove"]]]]]))]]]]]]])
 
 (defn app-page [id]
   [:div {:class "container"}
     [:div {:class "row row-cards row-deck"}
       [:div {:class "col-12"}
         (let [app @(rf/subscribe [:app (keyword id)])
+              tasks @(rf/subscribe [:app-tasks (keyword id)])
               loading? @(rf/subscribe [:loading?])]
         [:div {:class "card"}
           [:div {:class "card-header"}
@@ -75,11 +87,28 @@
           [alerts/for-card [:alert-dashboard]]
           [:div {:class "card-body"}
             [:div {:class "row"}
-              [:div {:class "col-2"} [:strong "ID:"]]
-              [:div {:class "col-5"} (:id app)]]
-            [:div {:class "row"}
-              [:div {:class "col-2"} [:strong "Installer ID:"]]
-              [:div {:class "col-5"} (:installer-id app)]]
-            [:div {:class "row"}
-              [:div {:class "col-2"} [:strong "Status:"]]
-              [:div {:class "col-5"} (:status app)]]]])]]])
+              [:div {:class "col-6"}
+                [:div {:class "row"}
+                  [:div {:class "col-2"} [:strong "ID:"]]
+                  [:div {:class "col-4"} (:id app)]]
+                [:div {:class "row"}
+                  [:div {:class "col-2"} [:strong "Installer ID:"]]
+                  [:div {:class "col-5"} (:installer-id app)]]
+                [:div {:class "row"}
+                  [:div {:class "col-2"} [:strong "Status:"]]
+                  [:div {:class "col-5"} (:status app)]]]
+              [:div {:class "col-6"}
+                [:div {:class "row"} [:strong "Tasks"]]
+                [:div {:class "list-group list-group-transparent mb-0"}
+                (for [{id :id name :name status :status progress :progress} (vals tasks)]
+                  [:div {:key id :class "row list-group-item list-group-item-action"} [:span {:class (str "status-icon bg-" (util/task-status-color status))}] name
+                    [:div {:class "col-4 mb-2"}
+                      [:div {:class "clearfix"} [:div {:class "float-left"} [:strong (str (:percentage progress) "%")]]]
+                      [:div {:class "progress progress-xs"}
+                        [:div {:class "progress-bar bg-green"
+                               :aria-valuemax "100"
+                               :aria-valuemin "0"
+                               :aria-valuenow (:percentage progress)
+                               :style {:width (str (:percentage progress) "%")}
+                               :role "progressbar"}]]]])]]]]])]]])
+
