38fd2a66098ecbefff6be9d8fb55fcc453ffe3ba
Alexandru Giurgiu <alex.giurgiu@gmail.com>
2019-10-08 19:56:56 +0300

    changed the source code link in the footer and updated the copyright

--- src/protosfrontend/components/footer.cljs
+++ src/protosfrontend/components/footer.cljs
@@ -12,6 +12,6 @@
             [:div {:class "col-auto"}
               [:a {:target "_blank" :href "https://releases.protos.io"} @(rf/subscribe [:instance-info])]]
             [:div {:class "col-auto"}
-              [:a {:class "btn btn-outline-primary btn-sm" :target "_blank" :href "https://github.com/protosio/protos"} "Source code"]]]]
-        [:div {:class "col-12 col-lg-auto mt-3 mt-lg-0 text-center"} "Copyright © 2018 "
+              [:a {:class "btn btn-outline-primary btn-sm" :target "_blank" :href "https://code.protos.io"} "Source code"]]]]
+        [:div {:class "col-12 col-lg-auto mt-3 mt-lg-0 text-center"} "Copyright © 2018-2019 "
           [:a {:target "_blank" :href "https://www.protos.io"} "Protos"]]]]])
